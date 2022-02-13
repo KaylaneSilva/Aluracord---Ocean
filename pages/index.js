dfdf641bc1b27bbd228c001561bafa42d33ba31b
kaylanerss <kaylane25@hotmail.com>
2022-02-13 20:18:48 -0300

    pegando mais informacoes e adicionando botao de pesquisa

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import React from 'react';
 import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-// import '../form.css';
+import Head from 'next/head';
 
-function Title({children, tag}) {
+function Title({ children, tag }) {
   const Tag = tag || "h1";
   return (
     <>
@@ -20,21 +20,28 @@ function Title({children, tag}) {
   )
 }
 
-async function fetchApi(username) {
-  const url = `https://api.github.com/users/${username}`;
-  const getObj = await fetch(url)
-  // const info = await getObj.json();
-  return [await getObj.json()]
-}
-
 export default function PaginaInicial() {
   const [username, setUsername] = React.useState('');
+  const [info, setInfo] = React.useState({});
   const route = useRouter();
-  const allInfo = fetchApi('KaylaneSilva');
-  
-  console.log(allInfo);
+  const [image, setImage] = React.useState('')
+  const url = `https://api.github.com/users/${username}`;
+
+  React.useEffect(() => {
+    fetch(url).then((response) => response.json())
+      .then((data) => console.log(data))
+  }, [])
+
   return (
     <>
+      <Head>
+        <meta charset="utf-8" />
+        <link rel="icon" type='imagem/png' href="https://cdn.discordapp.com/attachments/935207613543624835/942544590077046814/3533679-200.png" />
+        <link rel="apple-touch-icon" href="https://cdn.discordapp.com/attachments/935207613543624835/942544590077046814/3533679-200.png" />
+        <title>
+          AluraCord - Ocean
+        </title>
+      </Head>
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -63,10 +70,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
-            onSubmit = { function(event) {
+            onSubmit={function (event) {
               event.preventDefault();
               route.push(`/chat?username=${username}`);
-            } }
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -77,23 +84,66 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
-            <TextField
-              fullWidth
-              textFieldColors={{
-                neutral: {
-                  textColor: appConfig.theme.colors.neutrals[200],
-                  mainColor: appConfig.theme.colors.neutrals[900],
-                  mainColorHighlight: appConfig.theme.colors.primary[500],
-                  backgroundColor: appConfig.theme.colors.neutrals[800],
+            <Box
+              styleSheet={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                flexDirection: {
+                  xs: 'column',
+                  sm: 'row',
                 },
+                width: '110%', maxWidth: '900px',
+                borderRadius: '5px', padding: '32px', margin: '16px',
               }}
-              value={ username }
-              onChange={ function({ target }) {
-                const value = target.value;
+            >
+              <TextField
+                fullWidth
+                textFieldColors={{
+                  neutral: {
+                    textColor: appConfig.theme.colors.neutrals[200],
+                    mainColor: appConfig.theme.colors.neutrals[900],
+                    mainColorHighlight: appConfig.theme.colors.primary[500],
+                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                  },
+                }}
+                value={username}
+                onChange={function ({ target }) {
+                  const value = target.value;
 
-                setUsername(value);
-              } }
-            />
+                  setUsername(value);
+                }}
+              />
+              <Button
+                type='button'
+                label='Search'
+                disabled={username.length < 2}
+                width='fit-content'
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["000"],
+                  mainColor: appConfig.theme.colors.primary[500],
+                  mainColorLight: appConfig.theme.colors.primary[400],
+                  mainColorStrong: appConfig.theme.colors.primary[600],
+                }}
+                styleSheet={{
+                  marginLeft: '2vw',
+                  marginTop: '-1.3vh'
+                }}
+                onClick={function () {
+                  fetch(url, {
+                    headers: {
+                      'Accept': 'application/vnd.github.v3+json'
+                    }
+                  })
+                    .then(response => response.json()) //Converting the response to a JSON object
+                    .then(data => {
+                      setInfo({ followers: data.followers, following: data.following, location: data.location })
+                    })
+                    .catch(error => console.error(error));
+                  setImage(`https://github.com/${username}.png`)
+                }}
+              />
+            </Box>
             <Button
               type='submit'
               label='Entrar'
@@ -116,7 +166,7 @@ export default function PaginaInicial() {
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
-              maxWidth: '200px',
+              maxWidth: '250px',
               padding: '16px',
               backgroundColor: appConfig.theme.colors.neutrals[800],
               border: '1px solid',
@@ -131,7 +181,7 @@ export default function PaginaInicial() {
                 borderRadius: '50%',
                 marginBottom: '16px',
               }}
-              src={`https://github.com/${username}.png`}
+              src={image}
             />
             <Text
               variant="body4"
@@ -143,6 +193,18 @@ export default function PaginaInicial() {
               }}
             >
               {username}
+            </Text>
+            <Text
+              variant="body4"
+              styleSheet={{
+                color: appConfig.theme.colors.neutrals[200],
+                backgroundColor: appConfig.theme.colors.neutrals[900],
+                padding: '3px 10px',
+                borderRadius: '1000px',
+                marginTop: '2vh'
+              }}
+            >
+              Followers: {info.followers} Following: {info.following}
             </Text>
           </Box>
           {/* Photo Area */}
